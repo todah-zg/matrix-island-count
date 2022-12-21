51dd285ecf65eea1b3aa511e67684134532b29f1
marijan <marijan@todah.hr>
2022-12-21 19:15:46 +0100

    basic main function for quick random test

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,23 @@ using namespace std;
 
 int main(void)
 {
-    int max = 30;
-    vector<vector<int>> v(max, vector<int>(max));
+    int max = 20;
+    srand(time(NULL));
+    int size = rand() % max;
+    vector<vector<int>> v(size, vector<int>(size));
 
-    for (int i = 0; i < max; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < max; j++)
+        for (int j = 0; j < size; j++)
         {
             v[i][j] = rand() % 2;
         }
     }
 
-    auto m = matrix::Matrix(v);
-    std::cout << m;
+    auto matrix = matrix::Matrix(v);
+    cout << "Generated " << size << " x " << size << " matrix:\n";
+    cout << matrix;
+    cout << "\n\nNumber of individual figures: " << matrix.countFigures() << "\n";
 
 return 0;
 
